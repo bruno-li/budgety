732e84ae6163643e63936ba6c9cc636eb7fe723b
Bruno Li <lisiuloong@gmail.com>
2019-08-27 21:26:10 -0400

    implemented event listener

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
-// IFFE returning an anonymous function
+/*jshint esversion: 6 */ 
+
+
+
+// BUDGET CONTROLLER
 var budgetController = (function(){
     //private properties. Not accessible outside the function
-    var x = 23;
-    var add = function(a){
-        return x + a;
-    }
+
 
 // return an object that will be available to be used outside this function. This can be done because of Closures.
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+
 
 }());
 
-// UI Controller Module
+// UI CONTROLLER
 var UIController = (function(){
     
 }());
 
 
 
-// App Controller Module. This is used to integrate budgedtController and UIController Modules
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
-    var z = budgetCtrl.publicTest(5);
-    
-    return {
-        anotherPublic: function(){
-            console.log(z);
+
+    var ctrlAddItem = function(){
+        // 1. get field input data
+
+        //2. add the item to the budget controller
+
+        //3. add the item to the UI
+
+        //4. calculate the budget
+
+        //5. Display the budget on the ui
+    };
+
+    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+
+    // Global document for return press. .which is for older broweserd that dosent support .keycode
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode === 13 || event.which === 13){
+          ctrlAddItem();
         }
-    }
-}(budgetController, UIController));
+    });
+})(budgetController,UIController);
